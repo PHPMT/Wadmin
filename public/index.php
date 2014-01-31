29b35383bb227318df5deb582d6a41c639ecd760
Jack Makiyama <jackmakiyama@gmail.com>
2014-01-31 01:58:34 -0300

    Acionado novas rotas

--- public/index.php
+++ public/index.php
@@ -23,10 +23,18 @@ date_default_timezone_set($app->timezone);
 setlocale(LC_ALL, 'pt_BR');
 define('APP_NAME', $app->app_name);
 
+$auth = new Common\Routine\Auth;
+$authenticated = function() use($auth) {return $auth();};
+
 $r = new Router;
 
-$r->get('/',  'Wadmin\Routes\GenericRoute');
-$r->get('/*', 'Wadmin\Routes\GenericRoute');
+$r->get('/login',  'Wadmin\Routes\Login');
+$r->post('/login', 'Wadmin\Routes\Login');
+$r->get('/logout', 'Wadmin\Routes\Logout');
+
+$r->get('/',       'Wadmin\Routes\Home')->by($authenticated);
+
+$r->any('/**', 'Wadmin\Routes\Error404');
 
 $r->always('Accept', array(
     'text/html' => new Common\Routine\Twig($app->twig)
